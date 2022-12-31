f518de416039f803509ac170a39e054e47f6a86b
Qazi Ahmad <107860183+QaziAhmad77@users.noreply.github.com>
2022-12-31 14:58:57 +0500

    Update index.js

--- controller/index.js
+++ controller/index.js
@@ -1,1 +1,7 @@
-module.exports = {};
+module.exports = async (req, res) => {
+  try {
+  } catch (err) {
+    console.log(err);
+    return res.status(err.status || 500).send(err.message || 'Something went wrong');
+  }
+};
