be6c23bb0befb980979a2b0208ab6531a0693938
Qazi Ahmad <107860183+QaziAhmad77@users.noreply.github.com>
2022-12-31 18:06:00 +0500

    Update tryCatch.js

--- controller/tryCatch.js
+++ controller/tryCatch.js
@@ -1,22 +1,27 @@
 const fetch = require('node-fetch');
 module.exports = async (req, res) => {
+  let { address } = req.query;
   try {
     const parseTitle = (body) => {
       let match = body.match(/<title>([^<]*)<\/title>/); // regular expression to parse contents of the <title> tag
       if (!match || typeof match[1] !== 'string') throw new Error('Unable to parse the title tag');
       return match[1];
     };
-    const { address } = req.query;
+
     if (!address) {
       return res.status(400).end('Missing address query parameter');
     }
+
     const response = await fetch(address);
     const body = await response.text();
     const title = parseTitle(body);
     if (!title) return res.status(400).end('Unable to parse title');
     return res.status(200).send(title);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).end('Something went wrong');
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send(
+      ` ${address} - NO RESPONSE  "
+    Only absolute URLs are supported"` || err.message
+    );
   }
 };
