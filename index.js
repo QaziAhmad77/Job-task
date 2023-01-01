096d781e8f342455b4cfad0c3984cc74e9ae5cc1
Qazi Ahmad <107860183+QaziAhmad77@users.noreply.github.com>
2023-01-01 06:48:31 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
-// const controller = require('./controller/asyncAwait'); // By async and await method check this one and commit the remaining 2
+const controller = require('./controller/asyncAwait'); // By async and await method check this one and commit the remaining 2
 // const controller = require('./controller/promiss'); // By prommise (resolve, reject) method check this one and commit the remaining 2
-const controller = require('./controller/thenCatch'); // By then and Catch method check this one and commit the remaining 2
+// const controller = require('./controller/thenCatch'); // By then and Catch method check this one and commit the remaining 2
 
 const handlebars = require('handlebars');
 const hbs = handlebars.create();
